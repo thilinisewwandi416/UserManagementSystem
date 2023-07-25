b13af5dd639806284496a7121d3cfbfff424e164
Thilini Sewwandi <thilini.sewwandi416@gmail.com>
2023-07-25 10:57:58 +0530

    user management system initial commit

---
+++ User Management/user_management/assets/js/index.js
@@ -0,0 +1,44 @@
+$("#add_user").submit(function(event){
+    alert("Data inserted Successfully!")
+})
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array=$(this).serializeArray();
+    var data={}
+
+    $.map(unindexed_array,function(n,i){
+        data[n['name']]=n['value']
+    })
+    console.log(data);
+
+    var request={
+        "url": "http://localhost:3000/api/users/" + data.id,
+        "method":"PUT",
+        "data":data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Data Updated Successfully")
+    })
+})
+
+if (window.location.pathname === "/") {
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id");
+
+        var request = {
+            "url": `http://localhost:3000/api/users/${id}`, // Use backticks for URL
+            "method": "DELETE"
+        };
+
+        if (confirm("Do you want to delete this record?")) {
+            $.ajax(request).done(function(response) {
+                alert("Data Deleted Successfully");
+                location.reload();
+            });
+        }
+    });
+}
